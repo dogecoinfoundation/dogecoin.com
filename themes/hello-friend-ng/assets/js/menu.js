fb798b97e4ea2f535a7cb1fe15842f7324e97e37
Dakoda Greaves <dakoda@xanimo.net>
2022-04-13 20:19:20 -0700

    move some javascript to main.js for bundling

--- themes/hello-friend-ng/assets/js/menu.js
+++ themes/hello-friend-ng/assets/js/menu.js
@@ -1,5 +1,4 @@
 // Mobile menu
-
 const menuTrigger = document.querySelector(".menu-trigger");
 const menu = document.querySelector(".menu");
 const mobileQuery = getComputedStyle(document.body).getPropertyValue(
@@ -21,30 +20,33 @@ menuTrigger &&
 
 window.addEventListener("resize", isMobileMenu);
 
+if (!window.location.toString().includes("dogepedia")) {
+  var rect = document.querySelector("#home").getBoundingClientRect();
+  var offset = {
+    top: rect.top + window.scrollY,
+    left: rect.left + window.scrollX,
+  };
+  var navbar = document.querySelector('header')
+  // Script to hide/show menu
+  var video = document.querySelector('#video-header');
+  window.onscroll = function () {
+    // pageYOffset or scrollY
+    
+    if (window.pageYOffset > rect.height) {
+        navbar.classList.add('scrolled')
+        video.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+        video.classList.remove('scrolled')
+    }
+  }
+}
+
 window.addEventListener('load', function () {
   if (screen.width <= 991) {
       document.querySelector('.menu__inner').addEventListener('click', function () {
           let menu = document.querySelector(".menu");
-          menu.classList.add('hidden');
+          menu.classList.toggle('hidden');
       });
   }
 });
-var navbar = document.querySelector('header')
-var rect = document.querySelector("#home").getBoundingClientRect();
-var offset = {
-  top: rect.top + window.scrollY,
-  left: rect.left + window.scrollX,
-};
-
-// Script to hide/show menu
-var video = document.querySelector('#video-header');
-window.onscroll = function () {
-  // pageYOffset or scrollY
-  if (window.pageYOffset > rect.height) {
-      navbar.classList.add('scrolled')
-      video.classList.add('scrolled')
-  } else {
-      navbar.classList.remove('scrolled')
-      video.classList.remove('scrolled')
-  }
-}
